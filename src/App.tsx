1999a8c0a2f3661c4c28e5cf2a28506dc1e8893e
Dan <dan.lewis@enspiral.com>
2020-01-03 11:46:59 +1300

    update settings to no root ref & inc. new resource

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { PostList, PostShow, PostCreate, PostEdit } from "./posts";
+import { ResourceTypesList, ResourceTypesShow, ResourceTypesCreate, ResourceTypesEdit } from "./resourceTypes";
 import { Admin, Resource } from "react-admin";
 import {
   FirebaseRealTimeSaga,
@@ -8,12 +9,13 @@ import {
   RAFirebaseOptions
 } from "react-admin-firebase";
 
-const config = require("./FIREBASE_CONFIG.js").firebaseConfig;
+const config = require("./FIREBASE_CONFIG.js").firebaseConfig
 
+// DL: check the options docs here: https://github.com/benwinding/react-admin-firebase/blob/ca95119b17b5ddad5ff0387a31873ba6f2df4f7c/README.md#options
 const options: RAFirebaseOptions = {
   logging: true,
-  rootRef: 'root_collection/some_document',
-  watch: ["posts"]
+  rootRef: '',
+  watch: ["posts", "resourceTypes"]
 }
 const dataProvider = FirebaseDataProvider(config, options);
 const authProvider = FirebaseAuthProvider(config, options);
@@ -33,6 +35,13 @@ class App extends React.Component {
           show={PostShow}
           create={PostCreate}
           edit={PostEdit}
+        />
+        <Resource
+          name="resources"
+          list={ResourceTypesList}
+          show={ResourceTypesShow}
+          create={ResourceTypesCreate}
+          edit={ResourceTypesEdit}
         />
       </Admin>
     );
